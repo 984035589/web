a84586a441189f0e1aefb8fa5de4cfcc9f4f4a99
liujian <984035589@qq.com>
2022-06-22 20:25:33 +0800

    feat: path

--- node/path/index.js
+++ node/path/index.js
@@ -21,3 +21,34 @@ const fileName = __filename;
 // console.log(path.extname("/a/b"));
 
 // 4. 解析路径
+// console.log(path.parse(fileName))
+// console.log(path.parse('/a/c/../s'))
+// console.log(path.parse('./a/c/s'))
+
+// 5. 序列化路径
+// console.log(path.format(path.parse(fileName)))
+
+// 6. 判断当前路径是否是绝对路径
+// console.log(path.isAbsolute('ff'))
+// console.log(path.isAbsolute('/ff'))
+// console.log(path.isAbsolute(fileName))
+// console.log(path.isAbsolute('./'))
+
+// 7. 路径拼接join
+// console.log(path.join("/a/b", "c", "index.html"));
+// console.log(path.join(__dirname, "/a/b", "c", "index.html"));
+// console.log(path.join(__dirname, "/a/b", "c",'../', "index.html"));
+// console.log(path.join(''))
+
+// 8. 规范化路径
+// console.log(path.normalize(''))
+// console.log(path.normalize('a/s/f/sad/.././sa'))
+// console.log(path.normalize('/a\s\s/dsa\f/dsa/das.../das/../'))
+// console.log(path.normalize('a//\b\c\\/d'))
+
+// 9. 拼接路径|绝对路径
+console.log(path.resolve("index.html"));
+console.log(path.resolve("/a", "./index.html"));
+console.log(path.resolve("a", "./index.html"));
+console.log(path.resolve("a", "index.html"));
+console.log(path.resolve("a", "../index.html"));
