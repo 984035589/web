bd02841704a15f55aaef8f3b7ae92f74ee1732d6
liujian <984035589@qq.com>
2022-07-19 23:59:36 +0800

    feat: koa db

---
+++ web-server/koa/koa-study/app/db/index.ts
@@ -0,0 +1,27 @@
+import { Sequelize } from 'sequelize-typescript';
+import config from '../config';
+import path from 'path';
+
+const sequelize = new Sequelize(config.db.db_name as string, config.db.db_user as string, config.db.db_password, {
+	port: config.db.db_port as unknown as number,
+	host: config.db.db_host,
+	dialect: 'mysql',
+	define: {
+		timestamps: true,
+		createdAt: 'created_at',
+		updatedAt: 'updated_at',
+		deletedAt: 'deleted_at'
+	},
+	models: [path.join(__dirname, '../model/**/*.ts'), path.join(__dirname, '../model/**/*.js')]
+});
+
+const db = async () => {
+	try {
+		await sequelize.authenticate();
+		console.log('Connection has been established successfully.');
+	} catch (error) {
+		console.error('Unable to connect to the database:', error);
+	}
+};
+
+export default db;
---
+++ web-server/koa/koa-study/app/model/Admin.ts
@@ -0,0 +1,7 @@
+import { Table, Model, Column } from 'sequelize-typescript';
+
+@Table
+export default class Admin extends Model {
+    @Column
+    name!: string
+}
