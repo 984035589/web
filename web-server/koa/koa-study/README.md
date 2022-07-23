baf4e7a0a6bfcf4d24ef41ef8ae206f4c9fcceb4
liujian <984035589@qq.com>
2022-07-23 14:07:56 +0800

    feat: https、session

---
+++ web-server/koa/koa-study/README.md
@@ -0,0 +1,39 @@
+# KOA
+
+## 1. koa、ts
+
+## 2. nodemon 热加载
+
+## 3. jest 单元测试
+
+## 4. dotenv 加载.env
+
+## 5. log4js 收集日志
+
+## 6. sequelize 数据库
+
+## 7. jwt 认证
+
+## 8. 统一 response
+
+## 9. 分页数据查询
+
+## 10. async-validator 校验端数据
+
+## 11. 文件上传
+
+## 12. crud
+
+## 13. 支持 https
+
+## 14. session
+
+## 15. 待办
+
+- 新增数据、修改数据方法有误
+- 文件下载
+- session 认证目前只做了 set-cookies
+- 文件下载
+- 打包
+- 发布到服务器
+- 制作镜像
--- web-server/koa/koa-study/app/index.ts
+++ web-server/koa/koa-study/app/index.ts
@@ -6,20 +6,29 @@ db();
 
 import Koa from 'koa';
 import KoaSession from 'koa-session';
+import fs from 'fs';
 import KoaStatic from 'koa-static';
 import koaBody from 'koa-body';
 import router from './router';
 import path from 'path';
 import sslify from 'koa-sslify';
+import config from './config';
+import https from 'https';
 
 import { Server } from 'http';
 import AccessLogMiddleware from './middleware/AccessLogMiddleware';
 
+const options = {
+	key: fs.readFileSync(path.join(__dirname, `../cert/${config.domain.name}.key`)),
+	cert: fs.readFileSync(path.join(__dirname, `../cert/${config.domain.name}_bundle.pem`))
+};
+
 const app = new Koa();
 
 app.keys = ['SECRET'];
-//
+
 app.use(sslify())
+	.use(KoaSession(config.session as any, app))
 	.use(KoaStatic(path.join(__dirname, '../static'), { gzip: true }))
 	.use(
 		koaBody({
@@ -31,7 +40,8 @@ app.use(sslify())
 	.use(router.routes());
 
 const run = (port: number): Server => {
-	return app.listen(port);
+	// return app.listen(port);
+	return https.createServer(options, app.callback()).listen(port);
 };
 
 export default run;
